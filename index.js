65634642df198381c2789e0887f6a9b0c448bb9a
milindrai <milindrai365@gmail.com>
2024-08-13 22:40:11 +0530

    Refactor index.js for better organization and readability

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-    }
+}
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 
@@ -27,42 +27,42 @@ const scopes = ['https://www.googleapis.com/auth/calendar'];
 
 // OAuth 2 configuration
 const oauth2Client = new google.auth.OAuth2
-(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
-); 
+    (
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URL
+    );
 
 app.get('/auth', (req, res) => {
 
     const url = oauth2Client.generateAuthUrl
-    ({
-        access_type: 'offline',
-        scope: scopes
-    });
+        ({
+            access_type: 'offline',
+            scope: scopes
+        });
     res.redirect(url);
-    }
+}
 );
 
 app.get("/auth/redirect", async (req, res) => {
 
-    const {tokens} = await oauth2Client.getToken(req.query.code);
+    const { tokens } = await oauth2Client.getToken(req.query.code);
     oauth2Client.setCredentials(tokens);
     res.send('Authentication successful! Please return to the console.');
-    }
+}
 
 );
 
 const calendar = google.calendar({
-    version: 'v3', 
+    version: 'v3',
     auth: oauth2Client
 });
 
 const event = {
-    summary: 'Tech Talk with Arindam',
+    summary: 'Tech Talk with Milind',
     location: 'Google Meet',
 
-    description: "Demo event for Arindam's Blog Post.",
+    description: "Demo event for Milind's Blog Post.",
     start: {
         dateTime: "2024-08-14T19:30:00+05:30",
         timeZone: 'Asia/Kolkata'
@@ -78,10 +78,10 @@ app.get('/create-event', async (req, res) => {
 
     try {
         const result = await calendar.events.insert({
-                                calendarId: 'primary', 
-                                auth:oauth2Client,
-                                resource: event
-                        });
+            calendarId: 'primary',
+            auth: oauth2Client,
+            resource: event
+        });
 
         res.send({
             status: 200,
@@ -91,6 +91,5 @@ app.get('/create-event', async (req, res) => {
         console.log(err);
         res.send(err);
     }
-    }
+}
 );
-
