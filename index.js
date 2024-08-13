87fa13938e627cef268a063d783b74b90313a6c3
milindrai <milindrai365@gmail.com>
2024-08-13 10:37:56 +0530

    index.js for better organization and readability

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-    }
+}
 );
 
 // Define the scope of access for the Google Calendar API.
@@ -18,28 +18,93 @@ const scopes = ['https://www.googleapis.com/auth/calendar'];
 
 // OAuth 2 configuration
 const oauth2Client = new google.auth.OAuth2
-(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
-); 
+    (
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URL
+    );
 
 app.get('/auth', (req, res) => {
 
     const url = oauth2Client.generateAuthUrl
-    ({
-        access_type: 'offline',
-        scope: scopes
-    });
+        ({
+            access_type: 'offline',
+            scope: scopes
+        });
     res.redirect(url);
-    }
+}
 );
 
 app.get("/auth/redirect", async (req, res) => {
 
-    const {tokens} = await oauth2Client.getToken(req.query.code);
+    const { tokens } = await oauth2Client.getToken(req.query.code);
     oauth2Client.setCredentials(tokens);
     res.send('Authentication successful! Please return to the console.');
-    }
+}
 
+);
+
+const calendar = google.calendar({
+    version: 'v3',
+    auth: oauth2Client
+});
+
+const event = {
+    summary: 'Tech Talk with Milind',
+    location: 'Google Meet',
+
+    description: "Demo event for Milind's Blog Post.",
+    start: {
+        dateTime: "2024-03-14T19:30:00+05:30",
+        timeZone: 'Asia/Kolkata'
+    },
+    end: {
+        dateTime: "2024-03-14T20:30:00+05:30",
+        timeZone: 'Asia/Kolkata'
+    },
+    colorId: 1,
+    conferenceData: {
+        createRequest: {
+            requestId: uuid(),
+        }
+    },
+
+    attendees: [
+        {email: 'milindrai365@gmail.com'},
+    ]
+
+};
+
+const result = await calendar.events.insert({
+    calendarId: 'primary', 
+    auth:oauth2Client , 
+    conferenceDataVersion: 1 , 
+    sendUpdates: 'all', 
+    resource: event
+});
+
+res.send({
+    status: 200,
+    message: 'Event created',
+    link: result.data.hangoutLink
+});
+
+app.get('/create-event', async (req, res) => {
+
+    try {
+        const result = await calendar.events.insert({
+            calendarId: 'primary',
+            auth: oauth2Client,
+            resource: event
+        });
+
+        res.send({
+            status: 200,
+            message: 'Event created',
+        });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+}
 );
