0435a93570c6c2828d702d2dadfb40fd4ff3bf9d
milindrai <milindrai365@gmail.com>
2024-08-13 10:30:57 +0530

    Refactor index.js for better organization and readability

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const express = require('express');
+const { google } = require('googleapis');
+const dotenv = require('dotenv');
 
 const app = express();
 
+dotenv.config();
+
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    }
+);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Define the scope of access for the Google Calendar API.
+const scopes = ['https://www.googleapis.com/auth/calendar'];
+
+// OAuth 2 configuration
+const oauth2Client = new google.auth.OAuth2
+(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+); 
+
+app.get('/auth', (req, res) => {
+
+    const url = oauth2Client.generateAuthUrl
+    ({
+        access_type: 'offline',
+        scope: scopes
+    });
+    res.redirect(url);
+    }
+);
+
+app.get("/auth/redirect", async (req, res) => {
+
+    const {tokens} = await oauth2Client.getToken(req.query.code);
+    oauth2Client.setCredentials(tokens);
+    res.send('Authentication successful! Please return to the console.');
+    }
+
+);
